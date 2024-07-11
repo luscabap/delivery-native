6016d0db03ba470d935b5a61ce254f17b03b97a4
luscabap <lucasbaptistasilva.dev@gmail.com>
2024-07-11 20:09:57 -0300

    :tada: Project Base Native

---
+++ src/app/index.tsx
@@ -0,0 +1,22 @@
+import { View, ScrollView } from "react-native";
+import { Header } from "../components/Header";
+
+import Constants from "expo-constants"
+import { Banner } from "../components/Banner";
+
+const statusBarHeight = Constants.statusBarHeight;
+
+export default function Index() {
+  return (
+    <ScrollView 
+      style={{ flex: 1 }} 
+      className="bg-slate-200" 
+      showsVerticalScrollIndicator={false}
+    > 
+      <View className="w-full px-2" style={{ marginTop: statusBarHeight + 8 }}>
+        <Header />
+        <Banner />
+      </View>
+    </ScrollView>
+  );
+}
